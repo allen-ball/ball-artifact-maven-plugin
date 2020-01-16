7bd4cdfd1a08c8aa9e7943305a376da9c8c38b6a
Allen D. Ball <ball@hcf.dev>
2020-01-16 06:42:04 +0000

    Removed javadoc/overview.html and moved comments to top-level package-info.java.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-artifact-maven-plugin/trunk@5037 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/maven/plugins/artifact/package-info.java
+++ src/main/java/ball/maven/plugins/artifact/package-info.java
@@ -1,14 +1,11 @@
 /*
  * $Id$
  *
- * Copyright 2019 Allen D. Ball.  All rights reserved.
+ * Copyright 2019, 2020 Allen D. Ball.  All rights reserved.
  */
 /**
  * artifact-maven-plugin {@link org.apache.maven.plugin.Mojo}s
  *
  * @author  <a href="mailto:ball@hcf.dev">Allen D. Ball</a>
  */
-/* @Manifest.Section */
 package ball.maven.plugins.artifact;
-
-/* import ball.annotation.Manifest; */
