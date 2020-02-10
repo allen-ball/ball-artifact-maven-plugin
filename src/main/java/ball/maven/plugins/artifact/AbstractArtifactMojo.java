4e59a17da1aaf7bc395016075e62b04f9876ea80
Allen D. Ball <ball@hcf.dev>
2020-02-10 04:57:54 +0000

    Removed download mojo.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-artifact-maven-plugin/trunk@5357 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/ball/maven/plugins/artifact/AbstractArtifactMojo.java
+++ src/main/java/ball/maven/plugins/artifact/AbstractArtifactMojo.java
@@ -131,9 +131,9 @@ public abstract class AbstractArtifactMojo extends AbstractMojo
     protected void set(AttachedArtifact artifact) {
         File file = getArtifactFile(artifact);
 
-        getLog().info(label(project.getArtifact().getClassifier(),
-                            project.getArtifact().getType())
-                      + " <-- " + file);
+        log.info(label(project.getArtifact().getClassifier(),
+                       project.getArtifact().getType())
+                 + " <-- " + file);
         project.getArtifact().setFile(file);
     }
 
@@ -146,7 +146,7 @@ public abstract class AbstractArtifactMojo extends AbstractMojo
     protected void attach(AttachedArtifact artifact) {
         File file = getArtifactFile(artifact);
 
-        getLog().info(label(artifact) + " <-- " + file);
+        log.info(label(artifact) + " <-- " + file);
         helper.attachArtifact(project,
                               defaultString(artifact.getType(), JAR),
                               artifact.getClassifier(),
